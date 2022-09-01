d3e48263202714d3a21ac5e34b359972a7f09ea4
Jenda Kolena <jendakolena@gmail.com>
2022-09-01 11:54:03 +0200

    fixup! ESP-WIFI-MESH impl
    send_block_time, recv, set_config, set_max_layer

--- src/espmesh/mod.rs
+++ src/espmesh/mod.rs
@@ -1,6 +1,8 @@
 use alloc::boxed::Box;
+use std::io::Write;
 use std::net::Ipv4Addr;
-use std::ptr::null;
+use std::ptr::{null, null_mut};
+use std::time::Duration;
 
 use ::log::info;
 use esp_idf_hal::mutex::Mutex;
@@ -9,7 +11,9 @@ use log::error;
 
 use types::State;
 
-use crate::espmesh::types::{MeshAddr, MeshData, MeshOpt, MeshTos};
+use crate::espmesh::types::{
+    MeshAddr, MeshConfig, MeshData, MeshOpt, MeshProto, MeshTos, RcvMessage,
+};
 
 static TAKEN: Mutex<bool> = Mutex::new(false);
 
@@ -84,7 +88,8 @@ impl EspMeshClient {
         }));
 
         let opt: *const mesh_opt_t = match opt {
-            Some(MeshOpt::SendGroup { addrs }) => {
+            Some(MeshOpt::SendGroup { addrs: _ }) => {
+                // TODO fix
                 let mut data: Vec<u8> = vec![];
                 Box::into_raw(Box::new(mesh_opt_t {
                     type_: MESH_OPT_SEND_GROUP as u8,
@@ -103,12 +108,135 @@ impl EspMeshClient {
         unsafe {
             let r = esp!(esp_mesh_send(addr, data, flag.into(), opt, 1));
 
-            std::mem::drop(Box::from_raw(addr));
-            std::mem::drop(Box::from_raw(data));
-            std::mem::drop(Box::from_raw(opt as *mut mesh_opt_t));
+            drop(Box::from_raw(addr));
+            drop(Box::from_raw(data));
+            drop(Box::from_raw(opt as *mut mesh_opt_t));
 
             r
         }
+    }
+
+    pub fn send_block_time(d: Duration) -> Result<(), EspError> {
+        esp!(unsafe { esp_mesh_send_block_time(d.as_millis() as u32) })
+    }
+
+    /// Receive a packet targeted to self over the mesh network
+    pub fn recv(&mut self, timeout: Duration) -> Result<RcvMessage, EspError> {
+        let rcv_addr = null_mut() as *mut mesh_addr_t;
+        let rcv_data = Box::into_raw(Box::new(mesh_data_t::default()));
+        let rcv_opt = Box::into_raw(Box::new(mesh_opt_t::default()));
+
+        let mut flag = 0;
+
+        let rcv_data = unsafe {
+            let r = esp!(esp_mesh_recv(
+                rcv_addr,
+                rcv_data,
+                timeout.as_millis() as i32,
+                &mut flag,
+                rcv_opt,
+                1
+            ));
+
+            // this is to fail if it should fail but still to release the memory potentially
+            // allocated by the structs
+
+            // TODO use
+            let _rcv_addr = Box::from_raw(rcv_addr);
+            let rcv_data = Box::from_raw(rcv_data);
+            let _rcv_opt = Box::from_raw(rcv_opt);
+
+            r?;
+
+            //     match *rcv_addr {
+            //     mesh_addr_t { mip: mip_t } => MeshAddr::MIP {
+            //         ip: mip.ip4.addr.into(),
+            //         port: mip.port,
+            //     },
+            //     mesh_addr_t { addr } => MeshAddr::Mac(addr.into()),
+            // };
+
+            Ok(rcv_data)
+        }?;
+
+        let from: MeshAddr = MeshAddr::Mac([0, 0, 0, 0, 0, 0]);
+
+        let data = unsafe {
+            Vec::from_raw_parts(
+                rcv_data.data,
+                rcv_data.size as usize,
+                rcv_data.size as usize,
+            )
+        };
+
+        let proto: MeshProto = unsafe { std::mem::transmute(rcv_data.proto as u8) };
+        let tos: MeshTos = unsafe { std::mem::transmute(rcv_data.tos as u8) };
+
+        Ok(RcvMessage {
+            from,
+            data: MeshData { data, proto, tos },
+            flag: flag as u16,
+        })
+    }
+
+    pub fn set_config(&mut self, config: MeshConfig) -> Result<(), EspError> {
+        let mesh_id = match config.mesh_id {
+            MeshAddr::Mac(raw) => mesh_addr_t { addr: raw },
+            MeshAddr::MIP { ip, port } => mesh_addr_t {
+                mip: mip_t {
+                    ip4: ip4_addr_t { addr: ip.into() },
+                    port,
+                },
+            },
+        };
+
+        let mut tmp = [0u8; 64];
+        (&mut tmp as &mut [u8])
+            .write(config.ap.password.as_bytes())
+            .expect("Can't copy bytes in memory");
+
+        let mesh_ap = mesh_ap_cfg_t {
+            password: tmp,
+            max_connection: config.ap.max_connection,
+            nonmesh_max_connection: config.ap.nonmesh_max_connection,
+        };
+
+        let mut tmp = [0u8; 64];
+        (&mut tmp as &mut [u8])
+            .write(config.router.password.as_bytes())
+            .expect("Can't copy bytes in memory");
+
+        let mut tmp2 = [0u8; 32];
+        (&mut tmp2 as &mut [u8])
+            .write(config.router.ssid.as_bytes())
+            .expect("Can't copy bytes in memory");
+
+        let router = mesh_router_t {
+            ssid: tmp2,
+            ssid_len: config.router.ssid.len() as u8,
+            password: tmp,
+            bssid: config.router.bssid,
+            allow_router_switch: config.router.allow_router_switch,
+        };
+
+        let cfg = Box::into_raw(Box::new(mesh_cfg_t {
+            mesh_id,
+            mesh_ap,
+            router,
+            channel: config.channel,
+            allow_channel_switch: config.allow_channel_switch,
+            crypto_funcs: null(),
+        }));
+
+        unsafe {
+            let r = esp!(esp_mesh_set_config(cfg));
+            drop(Box::from_raw(cfg));
+            r
+        }
+    }
+
+    pub fn set_max_layer(&mut self, max_layer: u16) -> Result<(), EspError> {
+        esp!(unsafe { esp_mesh_set_max_layer(max_layer as i32) })
     }
 }
 
--- src/espmesh/types.rs
+++ src/espmesh/types.rs
@@ -51,3 +51,51 @@ pub enum MeshOpt {
     /// return a remote IP address
     RecvDsAddr { ip: Ipv4Addr },
 }
+
+pub struct RcvMessage {
+    pub from: MeshAddr,
+    pub data: MeshData,
+    pub flag: u16,
+}
+
+/// Mesh router configuration
+pub struct MeshRouterConfig {
+    /// SSID
+    pub ssid: String,
+    /// password
+    pub password: String,
+    /// BSSID, if this value is specified, users should also specify \"allow_router_switch\".
+    pub bssid: [u8; 6],
+    /// if the BSSID is specified and this value is also set, when the router of this specified BSSID
+    /// fails to be found after \"fail\" (mesh_attempts_t) times, the whole network is allowed to switch
+    /// to another router with the same SSID. The new router might also be on a different channel.
+    /// The default value is false.
+    /// There is a risk that if the password is different between the new switched router and the previous
+    /// one, the mesh network could be established but the root will never connect to the new switched router.
+    pub allow_router_switch: bool,
+}
+
+pub struct MeshApConfig {
+    // mesh softAP password
+    pub password: String,
+    // max mesh connections
+    pub max_connection: u8,
+    // max non-mesh connections
+    pub nonmesh_max_connection: u8,
+}
+
+/// Mesh initialization configuration
+pub struct MeshConfig {
+    pub channel: u8,
+    /// if this value is set, when \"fail\" (mesh_attempts_t) times is reached, device will change to
+    /// a full channel scan for a network that could join. The default value is false.
+    pub allow_channel_switch: bool,
+    /// mesh network identification
+    pub mesh_id: MeshAddr,
+    /// router configuration
+    pub router: MeshRouterConfig,
+    /// mesh softAP configuration
+    pub ap: MeshApConfig,
+    // /// crypto functions
+    // pub crypto_funcs: *const mesh_crypto_funcs_t,
+}
