61b264c663f406194e518e0ca894d064341cfd7e
Jenda Kolena <jendakolena@gmail.com>
2022-09-03 23:58:39 +0200

    fixup! ESP-WIFI-MESH impl
    set_ap_authmode, set_ap_connections, is_root, get_routing_table_size, get_routing_table
    
    send and recv working

--- src/espmesh/types.rs
+++ src/espmesh/types.rs
@@ -12,9 +12,24 @@ pub enum State {
     Stopped,
 }
 
+#[derive(Copy, Clone)]
 pub enum MeshAddr {
     Mac([u8; 6]),
     MIP { ip: Ipv4Addr, port: u16 },
+}
+
+impl From<MeshAddr> for mesh_addr_t {
+    fn from(addr: MeshAddr) -> Self {
+        match addr {
+            MeshAddr::Mac(raw) => mesh_addr_t { addr: raw },
+            MeshAddr::MIP { ip, port } => mesh_addr_t {
+                mip: mip_t {
+                    ip4: ip4_addr_t { addr: ip.into() },
+                    port,
+                },
+            },
+        }
+    }
 }
 
 /// Protocol of transmitted application data.
@@ -61,11 +76,11 @@ pub struct RcvMessage {
 /// Mesh router configuration
 pub struct MeshRouterConfig {
     /// SSID
-    pub ssid: String,
+    pub ssid: &'static str,
     /// password
-    pub password: String,
+    pub password: &'static str,
     /// BSSID, if this value is specified, users should also specify \"allow_router_switch\".
-    pub bssid: [u8; 6],
+    pub bssid: Option<[u8; 6]>,
     /// if the BSSID is specified and this value is also set, when the router of this specified BSSID
     /// fails to be found after \"fail\" (mesh_attempts_t) times, the whole network is allowed to switch
     /// to another router with the same SSID. The new router might also be on a different channel.
@@ -77,7 +92,7 @@ pub struct MeshRouterConfig {
 
 pub struct MeshApConfig {
     // mesh softAP password
-    pub password: String,
+    pub password: &'static str,
     // max mesh connections
     pub max_connection: u8,
     // max non-mesh connections
@@ -98,4 +113,72 @@ pub struct MeshConfig {
     pub ap: MeshApConfig,
     // /// crypto functions
     // pub crypto_funcs: *const mesh_crypto_funcs_t,
+}
+
+#[derive(Debug)]
+pub enum MeshEvent {
+    /// mesh is started
+    MeshEventStarted = 0,
+    /// mesh is stopped
+    MeshEventStopped = 1,
+    /// channel switch
+    MeshEventChannelSwitch = 2,
+    /// a child is connected on softAP interface
+    MeshEventChildConnected = 3,
+    /// a child is disconnected on softAP interface
+    MeshEventChildDisconnected = 4,
+    /// routing table is changed by adding newly joined children
+    MeshEventRoutingTableAdd = 5,
+    /// routing table is changed by removing leave children
+    MeshEventRoutingTableRemove = 6,
+    /// parent is connected on station interface
+    MeshEventParentConnected = 7,
+    /// parent is disconnected on station interface
+    MeshEventParentDisconnected = 8,
+    /// no parent found
+    MeshEventNoParentFound = 9,
+    /// layer changes over the mesh network
+    MeshEventLayerChange = 10,
+    /// state represents whether the root is able to access external IP network
+    MeshEventTodsState = 11,
+    /// the process of voting a new root is started either by children or by the root
+    MeshEventVoteStarted = 12,
+    /// the process of voting a new root is stopped
+    MeshEventVoteStopped = 13,
+    /// the root address is obtained. It is posted by mesh stack automatically.
+    MeshEventRootAddress = 14,
+    /// root switch request sent from a new voted root candidate
+    MeshEventRootSwitchReq = 15,
+    /// root switch acknowledgment responds the above request sent from current root
+    MeshEventRootSwitchAck = 16,
+    /// the root is asked yield by a more powerful existing root. If self organized is disabled
+    /// and this device is specified to be a root by users, users should set a new parent
+    /// for this device. if self organized is enabled, this device will find a new parent
+    /// by itself, users could ignore this event.
+    MeshEventRootAskedYield = 17,
+    /// when devices join a network, if the setting of Fixed Root for one device is different
+    /// from that of its parent, the device will update the setting the same as its parent's.
+    /// Fixed Root Setting of each device is variable as that setting changes of the root.
+    MeshEventRootFixed = 18,
+    /// if self-organized networking is disabled, user can call esp_wifi_scan_start() to trigger
+    /// this event, and add the corresponding scan done handler in this event.
+    MeshEventScanDone = 19,
+    /// network state, such as whether current mesh network has a root.
+    MeshEventNetworkState = 20,
+    /// the root stops reconnecting to the router and non-root devices stop reconnecting to their parents.
+    MeshEventStopReconnection = 21,
+    /// when the channel field in mesh configuration is set to zero, mesh stack will perform a
+    /// full channel scan to find a mesh network that can join, and return the channel value
+    /// after finding it.
+    MeshEventFindNetwork = 22,
+    /// if users specify BSSID of the router in mesh configuration, when the root connects to another
+    /// router with the same SSID, this event will be posted and the new router information is attached.
+    MeshEventRouterSwitch = 23,
+    /// parent duty
+    MeshEventPsParentDuty = 24,
+    /// child duty
+    MeshEventPsChildDuty = 25,
+    /// device duty
+    MeshEventPsDeviceDuty = 26,
+    MeshEventMax = 27,
 }
